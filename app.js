8e97d864b5ddca10d19869c3139f0cc785bb161c
Olivia Lewis <ollybaby12@gmail.com>
2021-01-04 21:44:32 -0500

    implemented videoResult functions

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 const imageRootURL= 'https://api.unsplash.com/search/photos?';
 const imageAPI= 'ep8A89YFGZbZ6LVOC0XiSQB1gz_t239jb4znTUynjyk';
+const videoRootURL= 'https://www.googleapis.com/youtube/v3/search?';
+const videoAPI= 'AIzaSyB_80hd_V9QuYE39fqQ75hIFpYrTApnNuM';
 let imagePageNumber = 1;
 
 function formatQueryParams(params){
@@ -19,6 +21,19 @@ function displayImageResults(responseJson){
             </li>`
         )};
     $('#image-results').removeClass('hidden');
+};
+
+function displayVideoReslts(responseJson){
+    $('#ivideo-result-list').empty();
+    for(let i = 0; i < responseJson.items.length; i++){
+        $('#video-result-list').append(
+            `<li>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}" 
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </li>`
+        )};
+    $('#video-results').removeClass('hidden');
 };
 
 function getReferenceImages(poseSearch){
@@ -45,14 +60,40 @@ function getReferenceImages(poseSearch){
     })
 }
 
+function getYoutubeVideos(poseSearch){
+    const params = {
+        key: videoAPI,
+        q: 'how to draw ' + poseSearch,
+        part:'snippet',
+        maxResults: 4,
+        videoEmbeddable: true,
+        type: 'video'
+    }
+
+    const queryString = formatQueryParams(params)
+    const videoUrl = videoRootURL + queryString
+    
+    fetch(videoUrl)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText)
+    })
+    .then(responseJson => displayVideoReslts(responseJson))
+    .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`)
+    })
+}
+
 function watchForm(){
     $('#js-form').submit(event => {
         event.preventDefault();
         const poseSearch= $('#js-pose').val();
         getReferenceImages(poseSearch);
+        getYoutubeVideos(poseSearch);
     })
 }
 $(function(){
-    console.log("App Loaded!")
     watchForm();
 })
